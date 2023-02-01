382c3cb9fc3e9b9799f7aa144c0d6597d97383a4
ayedFNFmakerSource code <123101780+ayedSourcecode@users.noreply.github.com>
2023-02-01 21:04:39 +0100

    Update CreditsState.hx

--- source/CreditsState.hx
+++ source/CreditsState.hx
@@ -81,6 +81,8 @@ class CreditsState extends MusicBeatState
 		#end
 
 		var pisspoop:Array<Array<String>> = [ //Name - Icon name - Description - Link - BG Color
+			['Vs Wastanky V2'],
+			['Wastanky', 'Wastanky', 'Owner', 'https://www.youtube.com/@wastankyIsnotHere/featured', 'FF1FCC'],
 			['Psych Engine Team'],
 			['Shadow Mario',		'shadowmario',		'Main Programmer of Psych Engine',								'https://twitter.com/Shadow_Mario_',	'444444'],
 			['RiverOaken',			'river',			'Main Artist/Animator of Psych Engine',							'https://twitter.com/RiverOaken',		'B42F71'],
